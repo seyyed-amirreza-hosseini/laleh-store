0fa2dbe0cd0a7cf5f89c73a37cee64d8d19f5699
seyyed_amirreza_hosseini <hosseiniseyyedamirreza2003@gmail.com>
2024-08-30 14:55:39 +0330

    Filter objects

--- playground/views.py
+++ playground/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
-
+from store.models import Product, Customer, Collection, Order, OrderItem
 
 def say_hello(request):
-    return render(request, 'hello.html', {'name': 'Amirreza'})
+    # Customers with .com account
+    queryset = Customer.objects.filter(email__icontains='.com')
+
+    # Collections that don't have a featured product
+    queryset = Collection.objects.filter(featured_product__isnull=True) 
+
+    # Products with low inventory (less than 10)
+    queryset = Product.objects.filter(inventory__lt=10)
+
+    # Orders placed by customer with id=1
+    queryset = Order.objects.filter(customer__id=1)
+
+    # Order items for products in collection 3
+    queryset = OrderItem.objects.filter(product__collection_id=3)
+
+    return render(request, 'hello.html', {'name': 'Amirreza', 'products': list(queryset)})
