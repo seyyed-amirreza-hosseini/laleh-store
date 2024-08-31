74ed83c64925a1f280be944baeb35f0958c05195
seyyed_amirreza_hosseini <hosseiniseyyedamirreza2003@gmail.com>
2024-08-31 18:09:38 +0330

    Add a computed column

--- store/admin.py
+++ store/admin.py
@@ -7,9 +7,15 @@ admin.site.register(models.Collection)
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title', 'unit_price']
+    list_display = ['title', 'unit_price', 'inventory_status']
     list_editable = ['unit_price']
     list_per_page = 10
+
+    @admin.display(ordering='inventory')
+    def inventory_status(self, product):
+        if product.inventory < 10:
+            return 'Low'
+        return 'Ok'
 
 
 @admin.register(models.Customer)
