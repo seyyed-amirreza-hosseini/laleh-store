54695e15ad91c86bbf42ae40f16b9c7893f2c88f
seyyed_amirreza_hosseini <hosseiniseyyedamirreza2003@gmail.com>
2024-09-01 12:23:27 +0330

    Select related objects

--- store/admin.py
+++ store/admin.py
@@ -7,9 +7,13 @@ admin.site.register(models.Collection)
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title', 'unit_price', 'inventory_status']
+    list_display = ['title', 'unit_price', 'inventory_status', 'collection_title']
     list_editable = ['unit_price']
+    list_select_related = ['collection']
     list_per_page = 10
+
+    def collection_title(self, product):
+        return product.collection.title
 
     @admin.display(ordering='inventory')
     def inventory_status(self, product):
@@ -24,3 +28,9 @@ class CustomerAdmin(admin.ModelAdmin):
     list_editable = ['membership']
     ordering = ['first_name', 'last_name']
     list_per_page = 10
+
+
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'placed_at', 'customer']
+    
