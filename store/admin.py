63dbf9c7008106e6af29841204b4fcfa2df4491d
seyyed_amirreza_hosseini <hosseiniseyyedamirreza2003@gmail.com>
2024-09-01 13:30:51 +0330

    Create custom actions

--- store/admin.py
+++ store/admin.py
@@ -1,4 +1,4 @@
-from django.contrib import admin
+from django.contrib import admin, messages
 from django.db.models import Count
 from django.urls import reverse
 from django.utils.html import format_html, urlencode
@@ -34,6 +34,7 @@ class CollectionAdmin(admin.ModelAdmin):
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
+    actions = ['clear_inventory']
     list_display = ['title', 'unit_price', 'inventory_status', 'collection_title']
     list_editable = ['unit_price']
     list_filter = ['collection', 'last_update', InventoryFilter]
@@ -48,7 +49,11 @@ class ProductAdmin(admin.ModelAdmin):
         if product.inventory < 10:
             return 'Low'
         return 'Ok'
-
+    
+    @admin.action(description='Clear inventory')
+    def clear_inventory(self, request, queryset):
+        updated_count = queryset.update(inventory=0)
+        self.message_user(request, f"{updated_count} products were successfully updated.", messages.ERROR)
 
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
