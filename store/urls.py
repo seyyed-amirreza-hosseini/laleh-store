48c9864cad4e3972b4fa0e99af67e8a97fe5df8f
seyyed_amirreza_hosseini <hosseiniseyyedamirreza2003@gmail.com>
2024-09-04 00:37:15 +0330

    Generate routers for viewsets

--- store/urls.py
+++ store/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
+from rest_framework.routers import DefaultRouter
 from . import views
 
 
-urlpatterns = [
-    path('products/', views.ProductList.as_view()),
-    path('products/<int:pk>', views.ProductDetail.as_view()),
-    path('collections/', views.CollectionList.as_view()),
-    path('collections/<int:pk>', views.CollectionDetail.as_view(), name='collection-detail'),
-]
+router = DefaultRouter()
+router.register('products', views.ProductViewSet)
+router.register('collections', views.CollectionViewSet)
+
+urlpatterns = router.urls
