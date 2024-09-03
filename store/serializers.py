2f78e2aad363fc30acc810426b3849276a82ca7a
seyyed_amirreza_hosseini <hosseiniseyyedamirreza2003@gmail.com>
2024-09-03 18:22:09 +0330

    Serializing relationships

--- store/serializers.py
+++ store/serializers.py
@@ -1,5 +1,11 @@
 from decimal import Decimal
 from rest_framework import serializers
+from .models import Product, Collection
+
+
+class CollectionSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    title = serializers.CharField(max_length=255)
 
 
 class ProductSerializer(serializers.Serializer):
@@ -7,6 +13,23 @@ class ProductSerializer(serializers.Serializer):
     title = serializers.CharField(max_length=255)
     price = serializers.DecimalField(max_digits=10, decimal_places=2, source='unit_price')    
     price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
+    
+    # 1. Primary key
+    # collection = serializers.PrimaryKeyRelatedField(
+    #     queryset=Collection.objects.all()
+    # )
 
-    def calculate_tax(self, product):
+    # 2. String
+    # collection = serializers.StringRelatedField()
+
+    # 3. Nested object
+    # collection = CollectionSerializer()
+
+    # 4. Hyperlink
+    collection = serializers.HyperlinkedRelatedField(
+        queryset = Collection.objects.all(),
+        view_name='collection-detail',
+    )
+
+    def calculate_tax(self, product: Product):
         return product.unit_price * Decimal(1.1)
--- store/views.py
+++ store/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .models import Product
-from .serializers import ProductSerializer
+from .models import Product, Collection
+from .serializers import ProductSerializer, CollectionSerializer
 
 
 @api_view()
 def product_list(request):
-    products = Product.objects.all()
-    serializer = ProductSerializer(products, many=True)
+    products = Product.objects.select_related('collection').all()
+    serializer = ProductSerializer(products, many=True, context={'request': request})
     return Response(serializer.data)
 
 
@@ -16,4 +16,11 @@ def product_list(request):
 def product_detail(request, id):
     product = get_object_or_404(Product, pk=id)
     serializer = ProductSerializer(product)
+    return Response(serializer.data)
+
+
+@api_view()
+def collection_detail(request, pk):
+    collection = get_object_or_404(Collection, pk=pk)
+    serializer = CollectionSerializer(collection)
     return Response(serializer.data)
