6aed3471857eb4da4d79a2ea6d1d8c72fef881a8
seyyed_amirreza_hosseini <hosseiniseyyedamirreza2003@gmail.com>
2024-09-03 18:58:59 +0330

    Deserializing objects

--- store/serializers.py
+++ store/serializers.py
@@ -12,7 +12,7 @@ class CollectionSerializer(serializers.ModelSerializer):
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ['id', 'title', 'unit_price', 'price_with_tax', 'collection']
+        fields = ['id', 'title', 'description', 'slug', 'inventory', 'unit_price', 'price_with_tax', 'collection']
 
     price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
 
--- store/views.py
+++ store/views.py
@@ -1,23 +1,36 @@
 from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from .models import Product, Collection
 from .serializers import ProductSerializer, CollectionSerializer
 
 
-@api_view()
+@api_view(['GET', 'POST'])
 def product_list(request):
-    products = Product.objects.select_related('collection').all()
-    serializer = ProductSerializer(products, many=True, context={'request': request})
-    return Response(serializer.data)
+    if request.method == 'GET':
+        products = Product.objects.select_related('collection').all()
+        serializer = ProductSerializer(products, many=True, context={'request': request})
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
 
-
-@api_view()
+@api_view(['GET', 'PUT'])
 def product_detail(request, id):
     product = get_object_or_404(Product, pk=id)
-    serializer = ProductSerializer(product)
-    return Response(serializer.data)
-
+    if request.method == 'GET':
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = ProductSerializer(product, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    
 
 @api_view()
 def collection_detail(request, pk):
