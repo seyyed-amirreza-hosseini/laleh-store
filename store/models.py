40cd3d9f575ac3d45ee940f05456affcfbc3c443
seyyed_amirreza_hosseini <hosseiniseyyedamirreza2003@gmail.com>
2024-09-06 22:38:01 +0330

    Add uuid field to cart

--- store/models.py
+++ store/models.py
@@ -1,6 +1,6 @@
 from django.core.validators import MinValueValidator
 from django.db import models
-
+from uuid import uuid4
 
 class Promotion(models.Model):
     description = models.CharField(max_length=255)
@@ -92,6 +92,7 @@ class OrderItem(models.Model):
 
 
 class Cart(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4)
     created_at = models.DateTimeField(auto_now_add=True)
 
 
