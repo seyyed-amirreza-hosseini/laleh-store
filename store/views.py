9c21bd440088fcfd6ea9715c66e02a8efc15ed01
seyyed_amirreza_hosseini <hosseiniseyyedamirreza2003@gmail.com>
2024-09-10 19:37:59 +0330

    Apply update and delete permissions for admins only

--- store/views.py
+++ store/views.py
@@ -98,7 +98,11 @@ class CustomerViewSet(ModelViewSet):
 
 
 class OrderViewSet(ModelViewSet):
-    permission_classes = [IsAuthenticated]
+    http_method_names = ['get', 'patch', 'delete', 'head', 'options']
+    def get_permissions(self):
+        if self.request.method in ['PATCH', 'DELETE']:
+            return [IsAdminUser()]
+        return [IsAuthenticated()]
 
     def create(self, request, *args, **kwargs):
         serializer = OrderCreateSerializer(
