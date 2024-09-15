2ae1a7a784b77c616ec3a282852fa1433da11554
amirreza <hosseiniseyyedamirreza2003@gmail.com>
2024-09-15 16:46:30 +0330

    Write test for authenticated users

--- store/tests/test_collections.py
+++ store/tests/test_collections.py
@@ -9,4 +9,11 @@ class TestCreateCollection:
         client = APIClient()
         response = client.post('/store/collections/', {'title': 'a'})
     
-        assert response.status_code == status.HTTP_401_UNAUTHORIZED 
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+    def test_if_user_is_not_admin_returns_403(self):
+        client = APIClient()
+        client.force_authenticate(user={})
+        response = client.post('/store/collections/', {'title': 'a'})
+
+        assert response.status_code == status.HTTP_403_FORBIDDEN
